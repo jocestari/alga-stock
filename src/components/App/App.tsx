7991d00e18435894db5b32664e25baab9c854162
James de Oliveira Cestari <james.cestari@mpt.mp.br>
2020-09-19 13:47:22 -0300

    Clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TestComponent from '../TestComponent'
-// import ClassComponet from '../ClassComponent'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <TestComponent name="Mundo!!!" />
-        {/* <ClassComponet name="James"/> */}
-      </header>
+      App
     </div>
   );
 }
