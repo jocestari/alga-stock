d29c5fc3fdb8d011fc9c6beacdc7b5ec436eb342
James de Oliveira Cestari <james.cestari@mpt.mp.br>
2020-09-21 13:59:59 -0300

    add style whith sass and node-sass package

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -2,24 +2,34 @@ import React from 'react'
 import './Table.scss'
 
 const Table = () => {
-    return <table>
+    return <table className="AppTable">
         <thead>
             <tr>
               <th>Product</th>                  
               <th>Price</th>                  
-              <th>Stock</th>                  
+              <th className="right">Stock</th>                  
             </tr>
         </thead>
         <tbody>
             <tr>
                 <td>Cookie</td>
                 <td>$1.25</td>
-                <td>23</td>
+                <td className="right">23</td>
             </tr>
             <tr>
                 <td>Milk</td>
                 <td>$0.99</td>
-                <td>10</td>
+                <td className="right">10</td>
+            </tr>
+            <tr>
+                <td>Cookie</td>
+                <td>$1.25</td>
+                <td className="right">23</td>
+            </tr>
+            <tr>
+                <td>Milk</td>
+                <td>$0.99</td>
+                <td className="right">10</td>
             </tr>
         </tbody>
     </table>
