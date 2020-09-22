4e5f6c60380ad7d1d3649f8de1909e83989e21b4
James de Oliveira Cestari <james.cestari@mpt.mp.br>
2020-09-21 23:18:13 -0300

    add form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,9 @@ import Container from '../../shared/Container'
 import './App.css';
 import Table, { TableHeader } from '../../shared/Table'
 import Products from '../../shared/Table/Table.mockdata'
+import Form from '../../shared/Form'
+import Input from '../../shared/Input'
+import Button from '../../shared/Button'
 
 const headers: TableHeader[] = [
   { key: 'id', value:'#'},
@@ -24,6 +27,29 @@ function App() {
           headers={headers}
           data={Products}
         />
+        
+        <Form title="Product form" onSubmit={console.log}>
+            <Input 
+              label="Name"
+              placeholder="Eg.: Cookie"
+            />
+            <Input 
+              label="Price"
+              type="number"
+              step="0.01"
+              min="0"
+              placeholder="Eg.: 1.25"
+            />
+            <Input 
+              label="Stock"
+              type="number"
+              min="0"
+              placeholder="Eg.: 15"
+            />
+            <Button>
+              Submit
+            </Button>
+        </Form>
       </Container>  
       
     </div>
