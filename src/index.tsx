ffefd681ea0c8ee9bf71b97fae17a93fdf9045c6
James de Oliveira Cestari <james.cestari@mpt.mp.br>
2020-09-23 21:06:07 -0300

    add axios

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import http from './utils/http'
 
+http.get('/posts')
+http.get('/posts/1')
+http.get('https://viacep.com.br/ws/29090210/json/')
 
 ReactDOM.render(
   <React.StrictMode>
