594493ea4a26b0e369c087bd4d48dbbdff1cfd77
James de Oliveira Cestari <james.cestari@mpt.mp.br>
2020-09-24 20:11:13 -0300

    mock server

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: 'http://localhost:3024'
+    baseURL: 'http://localhost:4010',
+    headers: {
+        authorization: 'Bearer 123'
+    }
+
 })
 
 export default http
